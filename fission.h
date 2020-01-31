3bde07cb3198dec57495f8fad974c87cda79ee19
Armin-Darjani <60473201+Armin-Darjani@users.noreply.github.com>
2020-01-31 23:12:41 +0330

    Fission

---
+++ fission.h
@@ -0,0 +1,16 @@
+#ifndef FISSION_H
+#define FISSION_H
+class fission
+{
+
+private:
+    float recLength;
+    float recWidth;
+    float vRadius;
+public:
+    fission();
+    fission(float l ,float w ,float r);
+    float MaxDistance();
+};
+
+#endif // FISSION_H
