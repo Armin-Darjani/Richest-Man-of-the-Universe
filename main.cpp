1854c32351ce626ba8aead91edd68c924c9d9f56
Armin-Darjani <60473201+Armin-Darjani@users.noreply.github.com>
2020-02-01 23:00:11 +0330

    changed for work with both fission and fusion

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>;
 #include "fission.h";
+#include "fusion.h";
+#include <cmath>
+#include <stdio.h>
+#include <fstream>
+#include "string.h"
 using namespace std;
 
 struct userInput{
@@ -11,27 +16,57 @@ struct userInput{
 
 int main(int argc, char *argv[])
 {
+
+    bool numOflines = false;
     userInput *user_input;
-    int inputLines;
-    cin >> inputLines;
-    user_input = new userInput[inputLines];
-    for(int i = 0 ; i < inputLines ; i++ ){
-        cin >> user_input[i].fissionOrFusion >> user_input[i].L_R1 >> user_input[i].W_R2 >> user_input[i].R_d;
+
+    ifstream myReadFile;
+    int num_of_lines;
+    int counter = 0;
+    myReadFile.open("C:/Users/Armin/Desktop/text.txt");
+    int output;
+    if (myReadFile.is_open()) {
+         while (!myReadFile.eof()) {
+             if(numOflines == false){
+                numOflines = true;
+                myReadFile >> num_of_lines;
+                cout << num_of_lines<<endl;
+                user_input = new userInput[num_of_lines];
+            }
+             else{
+               myReadFile >> user_input[counter].fissionOrFusion >> user_input[counter].L_R1 >> user_input[counter].W_R2 >> user_input[counter].R_d;
+              // cout << user_input[counter].fissionOrFusion << user_input[counter].L_R1 << user_input[counter].W_R2 << user_input[counter].R_d<< endl;
+               counter = counter + 1;
+             }
+         }
     }
+
     fission *fissionOperation;
+    fusion  *fusinOperation;
     float outFission;
-    for(int i = 0 ; i < inputLines ; i++ ){
+    float outFusion;
+    for(int i = 0 ; i < num_of_lines ; i++ ){
         if(user_input[i].fissionOrFusion == 'S'){
             fissionOperation = new fission(user_input[i].L_R1 , user_input[i].W_R2 ,user_input[i].R_d);
             outFission = fissionOperation->MaxDistance();
             if(outFission == -1)
-                cout << "Not enough space for fission"<<endl << endl;
+                cout << "Not enough space for fission."<<endl << endl;
             else
                 cout << outFission << endl<<endl;
             delete fissionOperation;
         }
+        else if(user_input[i].fissionOrFusion == 'M'){
+            fusinOperation = new fusion(user_input[i].L_R1 , user_input[i].W_R2 ,user_input[i].R_d);
+            outFusion = fusinOperation->CompactionRatio();
+            if(outFusion == 1.0000)
+                printf("%.4f \nNo compaction has occured.\n" , outFusion);
+            else
+                cout << outFusion << endl<<endl;
+            delete fusinOperation;
+        }
+        else
+            continue;
     }
-
     while(1){};
 
     return 0;
